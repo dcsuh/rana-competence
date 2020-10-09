ebe6704a53bca94cb6cae3c1c0793c71bdca1be3
Daniel Suh <ds91149@uga.edu>
2020-10-09 11:48:16 -0400

    packages and data

--- scripts/figures/fig7_code.R
+++ scripts/figures/fig7_code.R
@@ -1,1 +1,14 @@
+#Script Originator: Daniel Suh
 
+#This script creates figure 7
+
+
+library(tidyverse)
+library(magrittr)
+library(vegan)
+library(reshape2)
+library(patchwork)
+library(ggnewscale)
+library(here)
+
+data <- read_csv(here("data/weighted_prev_competence.csv"))
