5e0ad616ca7c057fc6a1b9d2018704e21a00ec4b
dcsuh <daniel.c.suh@gmail.com>
2021-03-19 11:23:03 -0400

    add test for geo additive gam

--- scripts/stats.R
+++ scripts/stats.R
@@ -11,7 +11,7 @@ source(knitr::purl(here("/scripts/data_format.Rmd"), quiet=TRUE))
 tmp <- data %>% 
   group_by(WetAltID, Month.1) %>% 
   summarize(infected = sum(RV.Status), total = n()) 
-tmp$Month <-   gsub("Month", "", tmp$Month.1)
+tmp$Month <-gsub("Month", "", tmp$Month.1)
 tmp %<>%
   add_column(., prev = tmp$infected/tmp$total) %>%
   mutate(., siteID = paste(WetAltID, Month, sep = "_")) 
@@ -32,14 +32,20 @@ Model <- glm(as.matrix(M)~lagged$lag_cc+lagged$lag_size+lagged$lag_temp, family=
 summary(Model)
 
 
+
+
+
 library(mgcv)
 library(biogeo)
 
 coords <- read_csv(here("/data/srs_wetland_coordinates.csv"))
 coords$dd_x <- dms2dd(coords$xdd,coords$xmm,coords$xss,coords$xdir)
 coords$dd_y <- dms2dd(coords$ydd,coords$ymm,coords$yss,coords$ydir)
+coords %<>% dplyr::select(WetAltID, dd_x, dd_y)
 
+geo_gam <- full_join(comm_summ, coords, by = "WetAltID") %>% dplyr::select(siteID, WetAltID, Month.1, cc, size, dd_x, dd_y)
+geo_gam$Month <- gsub("Month", "", geo_gam$Month.1)
+geo_gam$Month <- as.factor(geo_gam$Month)
 
-
-gam1<-gam(cc~te(x,y)+s(pop2016)+s(mean)+s(hi)+s(statefips,bs="re"),family="binomial",data=comm_summ)
+gam1<-gam(cc~te(dd_x,dd_y)+s(Month,bs="re")+s(log(size)),family="gamma",data=geo_gam)
 summary(gam1)
