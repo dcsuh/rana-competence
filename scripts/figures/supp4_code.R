428d1b97e915e362011fb1001af56743261dadcd
Daniel Suh <ds91149@uga.edu>
2021-03-29 08:54:09 -0400

    make supp4

--- scripts/figures/supp4_code.R
+++ scripts/figures/supp4_code.R
@@ -3,3 +3,14 @@
 
 #This script creates supplementary figure 4: prevalence ~ richness
 
+library(here)
+
+
+source(knitr::purl(here("/scripts/data_format.Rmd"), quiet=TRUE))
+
+
+evenness %>% ggplot(.,aes(x=richness, y=Prevalence)) +
+  geom_point() +
+  geom_smooth(method="lm")
+
+cor.test(evenness$richness, evenness$Prevalence, method="spearm")
