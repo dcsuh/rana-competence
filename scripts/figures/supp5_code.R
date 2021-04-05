3e8dabedfe91ec856c977a14fce119d57925e273
Daniel Suh <ds91149@uga.edu>
2021-04-05 12:31:48 -0400

    add corr test

--- scripts/figures/supp5_code.R
+++ scripts/figures/supp5_code.R
@@ -11,6 +11,11 @@ source(knitr::purl(here("scripts/data_format.Rmd"), quiet=TRUE))
 
 evenness$Month <- factor(evenness$Month,levels=c("Feb","Mar","Apr","May","Jun","Jul"))
 
-evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point() #+ geom_line(aes(group=WetAltID))
+evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point()
 
-evenness %>% ggplot(.,aes(x=log10(size),y=cc)) + geom_point()
+evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point() + geom_line(aes(group=WetAltID))
+
+evenness %>% ggplot(.,aes(x=log10(size),y=cc)) + geom_point() + geom_smooth(method="lm")
+cor.test(log10(evenness$size), evenness$cc, method = "spearman")
+  
+  
