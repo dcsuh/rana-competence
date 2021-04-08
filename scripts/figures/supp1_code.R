05aa4b9bfe0bccfb08b90e82df09ed4078958eb2
dcsuh <daniel.c.suh@gmail.com>
2021-04-08 15:21:04 -0400

    make supp1 png

--- scripts/figures/supp1_code.R
+++ scripts/figures/supp1_code.R
@@ -8,3 +8,12 @@ library(here)
 
 source(knitr::purl(here("/scripts/data_format.Rmd"), quiet=TRUE))
 
+vl %>% arrange(log10_value) %>% 
+  mutate(abb_name = factor(abb_name, levels = abb_name)) %>% 
+  ggplot(.,aes(x=abb_name,y=log10_value)) +
+  geom_point() + 
+  xlab("Species") + ylab("log10(Viral Load)") + 
+  geom_hline(yintercept = log10(20)) + 
+  labs(title = "Viral Loads") + 
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
