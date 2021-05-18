b0384e9e392fcd7960b30eeceebf0ccca002b057
dcsuh <daniel.c.suh@gmail.com>
2021-05-18 19:26:15 -0400

    filter out species that are not present in a site-month

--- scripts/figures/supp6_code.R
+++ scripts/figures/supp6_code.R
@@ -36,20 +36,25 @@ y %<>% mutate(mpd=-999,min.pd=-999,mpdAbund=-999,min.pdAbund=-999)
 #so row numbers should equal number of distinct species * number of site-months
 #i.e. y = 16 species * 96 site-months = 1536 rows
 
-
-for (k in 1:dim(y)[1]){
-  tmp <- y %>% filter(siteID==y$siteID[k])
-  pd.row <- which(rownames(pd)==y$Species[k])
-  otherSpp <- tmp %>% filter(Species!=y$Species[k]) %>% select(Species) %>% pull()
-  pd.cols <- which(rownames(pd) %in% otherSpp)
-  myDists <- pd[pd.row,pd.cols]
-  mpd <- mean(myDists)
-  min.pd <- min(myDists)
-  y$mpd[k] <- mpd
-  y$min.pd[k] <- min.pd
-  tmp2 <- tmp %>% filter(Species!=y$Species[k])
-  tmp2 %<>% filter(relAbund == max(relAbund))
-  pd.colsAbund <- which(rownames(pd) %in% tmp2$Species)
+y %<>% filter(abund>0) #shouldn't we filter out any species that were absent? (DS 5/18/21)
+#the mean pd and min pd should just be calculated for species that are present but without 
+#filtering out those absent then you calculate mean pd and min pd for all of the possible 16 species
+for (k in 1:dim(y)[1]){ #loop through each row
+  #get mean pd and min pd for each row
+  tmp <- y %>% filter(siteID==y$siteID[k]) #filter for single site-month
+  pd.row <- which(rownames(pd)==y$Species[k]) #select rows from pd matching site-month
+  #select species that are not the species from that row
+  otherSpp <- tmp %>% filter(Species!=y$Species[k]) %>% select(Species) %>% pull() 
+  pd.cols <- which(rownames(pd) %in% otherSpp) #select pd for not focal species
+  myDists <- pd[pd.row,pd.cols] #get distances between focal species and other species
+  mpd <- mean(myDists) #calculate mean pd
+  min.pd <- min(myDists) #calculate min pd
+  y$mpd[k] <- mpd #add to df
+  y$min.pd[k] <- min.pd #add to df
+  #do same thing for relative abundances of each species in each site-month
+  tmp2 <- tmp %>% filter(Species!=y$Species[k]) #make tmp df for species that are not focal species
+  tmp2 %<>% filter(relAbund == max(relAbund)) #filter for max relative abundance
+  pd.colsAbund <- which(rownames(pd) %in% tmp2$Species) 
   myDistsAbund <- pd[pd.row,pd.colsAbund]
   mpdAbund <- mean(myDistsAbund)
   min.pdAbund <- min(myDistsAbund)
@@ -57,9 +62,23 @@ for (k in 1:dim(y)[1]){
   y$min.pdAbund[k] <- min.pdAbund
 }
 
-y %>% filter(relAbund>0) %>% ggplot(.,aes(x=mpd,y=relAbund))+geom_point()+geom_smooth(method="loess",span=1.4)
-supp6 <- y %>% filter(relAbund>0) %>% ggplot(.,aes(x=min.pd,y=relAbund))+geom_point()+geom_smooth(method="loess",span=1.4)
-y %>% filter(relAbund>0) %>% ggplot(.,aes(x=mpdAbund,y=relAbund))+geom_point()+geom_smooth(method="loess",span=1.4)
-y %>% filter(relAbund>0) %>% ggplot(.,aes(x=min.pdAbund,y=relAbund))+geom_point()+geom_smooth(method="loess",span=1.4)
+#each point is one species in one site-month
+#min.pd is the 
+y %>% filter(relAbund>0) %>% ggplot(.,aes(x=mpd,y=relAbund))+
+  geom_point()+geom_smooth(method="loess",span=1.4)
+#above line gives us the relative abundance of each species and the mean pd for their site-month
+#hosts in the same site-month should be stacked vertically
+supp6 <- y %>% filter(relAbund>0) %>% ggplot(.,aes(x=min.pd,y=relAbund))+
+  geom_point()+geom_smooth(method="loess",span=1.4)
+supp6
+#supp6 gives us the relative abundance for each species and the pd for their closest phylogenetic neighbor in their site-month
+#so each vertical stack should be a species' relative abundance in all 
+y %>% filter(relAbund>0) %>% ggplot(.,aes(x=mpdAbund,y=relAbund))+
+  geom_point()+geom_smooth(method="loess",span=1.4)
+y %>% filter(relAbund>0) %>% ggplot(.,aes(x=min.pdAbund,y=relAbund))+
+  geom_point()+geom_smooth(method="loess",span=1.4)
 
-ggsave("supp6.png",plot=supp6,device="png",path=here("figures"))
+
+
+
+#ggsave("supp6.png",plot=supp6,device="png",path=here("figures"))
