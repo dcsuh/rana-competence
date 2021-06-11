095aee10710553beb06c65bf3d145625e835dcc7
dcsuh <daniel.c.suh@gmail.com>
2021-06-11 11:58:09 -0700

    update figures

--- scripts/figures/supp3_code.R
+++ scripts/figures/supp3_code.R
@@ -11,11 +11,11 @@ source(knitr::purl(here("scripts/data_format.Rmd"), quiet=TRUE))
 
 evenness$Month <- factor(evenness$Month,levels=c("Feb","Mar","Apr","May","Jun","Jul"))
 
-supp5a <- evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point() + ylab("Community Competence")
+supp3a <- evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point() + ylab("Community Competence")
 
 evenness %>% ggplot(.,aes(x=Month,y=cc)) + geom_boxplot() + geom_point() + geom_line(aes(group=WetAltID))
 
-supp5b <- evenness %>% ggplot(.,aes(x=log10(size),y=cc)) + geom_point() + geom_smooth(method="lm")
+supp3b <- evenness %>% ggplot(.,aes(x=log10(size),y=cc)) + geom_point() + geom_smooth(method="lm")
 cor.test(log10(evenness$size), evenness$cc, method = "spearman")
   
 ggsave("supp3a.png",plot=supp3a,device="png",path=here("figures"))
