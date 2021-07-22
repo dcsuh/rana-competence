ccf94f03a4489738471e42fd1c054c8b2757b9ea
dcsuh <daniel.c.suh@gmail.com>
2021-07-22 12:53:24 -0400

    update fig 5 text size

--- scripts/figures/fig5_code.R
+++ scripts/figures/fig5_code.R
@@ -8,13 +8,17 @@ library(here)
 
 source(knitr::purl(here("scripts/data_format.Rmd"), quiet=TRUE))
 
+axis_title_size = 18
+axis_text_size = 15
 
 #plot cleaned plot with lag
 cc_corr <- clean %>% ggplot(.,aes(x=lag_cc, y=Prevalence)) +
   geom_point() +
   theme_classic() + geom_smooth(method = "lm") +
-  labs(title = "", x = "Community Competence", y = "Prevalence (t+1)") +
-  ylim(0,0.65)
+  labs(title = "", x = "Community Competence (CC)", y = "Prevalence (t+1)") +
+  ylim(0,0.65) +
+  theme(axis.title = element_text(size=axis_title_size),
+        axis.text = element_text(size=axis_text_size))
 
 
 
@@ -25,7 +29,9 @@ size_corr <- clean %>% ggplot(.,aes(x=log10(lag_size), y=Prevalence)) +
   geom_point() +
   theme_classic() + geom_smooth(method = "lm") +
   labs(title = "", x = "log(Community Size)", y = "") +
-  theme(axis.text.y = element_blank(), axis.ticks.y=element_blank()) +
+  theme(axis.text.y = element_blank(), axis.ticks.y=element_blank(), 
+        axis.title = element_text(size=axis_title_size),
+        axis.text = element_text(size=axis_text_size)) +
   ylim(0,0.65)
 
 #cor.test(clean$lag_size,clean$Prevalence,method="spearman")
@@ -35,7 +41,9 @@ temp_corr <- clean %>% ggplot(.,aes(x=lag_temp, y=Prevalence)) +
   geom_point() +
   theme_classic() + geom_smooth(method = "lm") +
   labs(title = "", x = "Mean Water Temp", y = "") +
-  theme(axis.text.y = element_blank(), axis.ticks.y=element_blank()) +
+  theme(axis.text.y = element_blank(), axis.ticks.y=element_blank(), 
+        axis.title = element_text(size=axis_title_size),
+        axis.text = element_text(size=axis_text_size)) +
   ylim(0,0.65)
 
 #cor.test(clean$lag_temp,clean$Prevalence,method="spearman")
@@ -43,21 +51,21 @@ temp_corr <- clean %>% ggplot(.,aes(x=lag_temp, y=Prevalence)) +
 
 #plot everything together with patchwork
 figure_5 <- cc_corr| size_corr| temp_corr
-#ggsave("fig_5.png",plot=figure_5,device="png",path=here("/figures"))
+ggsave("fig_5.png",plot=figure_5,device="png",path=here("figures"))
 
 
 
 #test with 0's filtered out
 
-tmp <- clean %>% filter(Prevalence>0)
-cor.test(tmp$lag_cc,tmp$Prevalence,method="spearm")
-cor.test(tmp$lag_size,tmp$Prevalence,method="spearm")
-cor.test(tmp$lag_temp,tmp$Prevalence,method="spearm")
-
-tmp %>% ggplot(.,aes(x=lag_temp, y=Prevalence)) +
-  geom_point() +
-  theme_classic() + geom_smooth(method = "lm") +
-  labs(title = "", x = "Mean Water Temp", y = "") +
-  theme(axis.text.y = element_blank(), axis.ticks.y=element_blank()) +
-  ylim(0,0.65)
+# tmp <- clean %>% filter(Prevalence>0)
+# cor.test(tmp$lag_cc,tmp$Prevalence,method="spearm")
+# cor.test(tmp$lag_size,tmp$Prevalence,method="spearm")
+# cor.test(tmp$lag_temp,tmp$Prevalence,method="spearm")
+# 
+# tmp %>% ggplot(.,aes(x=lag_temp, y=Prevalence)) +
+#   geom_point() +
+#   theme_classic() + geom_smooth(method = "lm") +
+#   labs(title = "", x = "Mean Water Temp", y = "") +
+#   theme(axis.text.y = element_blank(), axis.ticks.y=element_blank()) +
+#   ylim(0,0.65)
 
