d00467c4443db89c633dcfbfb8ef1f609ab7c83f
dcsuh <daniel.c.suh@gmail.com>
2022-02-23 17:11:17 -0500

    update supp 5

--- scripts/figures/supp5_code.R
+++ scripts/figures/supp5_code.R
@@ -5,25 +5,62 @@
 
 library(here)
 
+source(here("base","src.R"))
 
-source(knitr::purl(here("scripts/data_format.Rmd"), quiet=TRUE))
+comm_data <- readRDS(here("processed_data","comm_data.rds"))
 
 
-supp5a <- evenness %>% ggplot(.,aes(x=richness, y=Prevalence)) +
+#create new df for ordered sites to test lagged richness and evenness on prevalence
+lag_evenness <- comm_data[-c(4,14,47,90),] #brute force remove site_months out of sequence
+lag_evenness$lag_richness <- c(0)
+lag_evenness$lag_J <- c(0)
+lag_evenness$lag_cc <- c(0)
+lag_evenness$lag_size <- c(0)
+for(n in 2:92){
+  lag_evenness$lag_richness[n] <- lag_evenness$richness[n-1]
+  lag_evenness$lag_J[n] <- lag_evenness$J[n-1]
+  lag_evenness$lag_cc[n] <- lag_evenness$cc[n-1]
+  lag_evenness$lag_size[n] <- lag_evenness$size[n-1]
+}
+
+#remove the first entry for each wetland to remove the carryover from the last wetland
+lag_evenness %<>% group_by(WetAltID) %>% filter(duplicated(WetAltID) | n()==1)
+
+#fit two lines to cc_evenness_prev plot
+lag_evenness$high <- c(0)
+lag_evenness$low <- c(0)
+for (i in 1:nrow(lag_evenness)){
+  if (lag_evenness$lag_J[i] > 0.6){
+    lag_evenness$high[i] <- 1
+    lag_evenness$low[i] <- 1
+  }
+  else if (lag_evenness$lag_cc[i] < 45000){
+    lag_evenness$low[i] <- 1
+  }
+  else if (lag_evenness$lag_cc[i] > 45000){
+    lag_evenness$high[i] <- 1
+  }
+  else {
+    lag_evenness$high[i] <- 0
+    lag_evenness$low[i] <- 0
+  }
+}
+
+supp5a <- comm_data %>% ggplot(.,aes(x=richness, y=Prevalence)) +
   geom_point() +
   geom_smooth(method="lm") +
   theme_classic() +
   labs(x="Richness", y="Prevalence")
 
-cor.test(evenness$richness, evenness$Prevalence, method="spearm")
+cor.test(comm_data$richness, comm_data$Prevalence, method="spearm")
 
-supp5b <- evenness %>% ggplot(.,aes(x=richness, y=cc)) +
+supp5b <- comm_data %>% ggplot(.,aes(x=richness, y=cc)) +
   geom_point() +
   geom_smooth(method="lm") +
   theme_classic() +
   labs(x="Richness", y="Community Competence")
 
-cor.test(evenness$richness, evenness$cc, method="spearm")
+cor.test(comm_data$richness, comm_data$cc, method="spearm")
 
 supp5c <- lag_evenness %>% ggplot(.,aes(x=lag_richness, y=Prevalence)) +
   geom_point() +
@@ -33,19 +70,19 @@ supp5c <- lag_evenness %>% ggplot(.,aes(x=lag_richness, y=Prevalence)) +
 
 cor.test(lag_evenness$lag_richness, lag_evenness$Prevalence, method="spearm")
 
-evenness %>% ggplot(.,aes(x=J, y=cc)) +
+comm_data %>% ggplot(.,aes(x=J, y=cc)) +
   geom_point() +
   geom_smooth(method="lm") +
   theme_classic() +
   labs(x="Evenness", y="Community Competence")
 
-evenness %>% ggplot(.,aes(x=J, y=Prevalence)) +
+comm_data %>% ggplot(.,aes(x=J, y=Prevalence)) +
   geom_point() +
   geom_smooth(method="lm") +
   theme_classic() +
   labs(x="Evenness", y="Prevalence")
 
-cor.test(evenness$J, evenness$cc, method="spearm")
+cor.test(comm_data$J, comm_data$cc, method="spearm")
 
 supp5d <- lag_evenness %>% ggplot(.,aes(x=lag_J, y=Prevalence)) +
   geom_point() +
