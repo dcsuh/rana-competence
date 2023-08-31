f9fbf295fadceaf526df24d220ff916b28d0e520
dcsuh <daniel.c.suh@gmail.com>
2023-08-31 11:37:20 -0700

    update supp1 point size

--- scripts/figures/supp1_code.R
+++ scripts/figures/supp1_code.R
@@ -13,13 +13,14 @@ vl <- readRDS(here("processed_data","vl.rds"))
 supp1 <- vl %>% filter(!is.na(mean)) %>% arrange(mean) %>% 
   mutate(abb_name = factor(abb_name, levels = abb_name)) %>% 
   ggplot(.,aes(x=abb_name,y=log10(mean))) +
-  geom_point() + 
+  geom_point(aes(size=1)) + 
   xlab("Species") + ylab("Viral Load") + 
   geom_hline(yintercept = 1) + 
   labs(title = "Viral Loads") + 
   theme_classic() + 
   theme(axis.text.x = element_text(angle = 30, hjust = 1,face = "italic", size = 12),
-        title = element_text(size = 18)) +
+        title = element_text(size = 18),
+        legend.position = "none") +
   scale_y_continuous(breaks=seq(0,6,1))
 
 supp1_se <- vl %>% filter(!is.na(mean)) %>% arrange(mean) %>% 
